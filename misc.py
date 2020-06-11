39f0d1b738d3c3e091ebc2c9f8ed9c7b99f9b445
Boyan Kovachev <boyankovachev40@gmail.com>
2020-06-11 22:05:11 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -20,3 +20,49 @@ def add_to_forest_table(conn, values):
     with conn:
         c = conn.cursor()
         c.execute(sql,values)
+
+def forest_table_to_str(conn):
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM forests")
+        rows = c.fetchall()
+        string = ''
+        i=0
+        for row in rows:
+            string = string + ''.join(str(rows[i]))
+            string = string + '\n'
+            i=i+1
+        return string
+
+def forest_type_table_to_str(conn):
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM forest_types")
+        rows = c.fetchall()
+        string = ''
+        i=0
+        for row in rows:
+            string = string + ''.join(str(rows[i]))
+            string = string + '\n'
+            i=i+1
+        return string
+
+def countries_table_to_str(conn):
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM countries")
+        rows = c.fetchall()
+        string = ''
+        i=0
+        for row in rows:
+            string = string + ''.join(str(rows[i]))
+            string = string + '\n'
+            i=i+1
+        return string
+
+def whole_db_to_string(conn):
+    string = forest_table_to_str(conn)
+    string = string + forest_type_table_to_str(conn)
+    string = string + countries_table_to_str(conn)
+    return string
+
